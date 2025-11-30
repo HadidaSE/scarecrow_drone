321410e91013a8ceb1ec82d81920c616971cc9b2
Itamar Hadida <hadidi@post.bgu.ac.il>
2025-11-30 15:02:17 +0200

    reaction time is down

--- drone_info.py
+++ drone_info.py
@@ -7,59 +7,25 @@ import time
 print "Connecting directly to flight controller on /dev/ttyS1..."
 
 try:
-    # Increased timeouts and added source_system parameter
     vehicle = connect('/dev/ttyS1',
                     wait_ready=False,
                     baud=1500000,
-                    heartbeat_timeout=30,
+                    heartbeat_timeout=10,
                     source_system=255)
 
-    print "Connected! Waiting for vehicle parameters..."
+    print "Connected! Fetching data..."
 
-    # Wait for specific attributes with longer timeout
-    vehicle.wait_ready('autopilot_version', timeout=30)
-
-    # Give extra time for all parameters to load
-    print "Loading parameters..."
-    time.sleep(3)
+    # Only wait for heartbeat, nothing else
+    time.sleep(1)
 
     print "\n=== DRONE INFO ==="
-
-    # Safely get each attribute with error handling
-    try:
-        print "Mode: %s" % vehicle.mode.name
-    except:
-        print "Mode: N/A"
-
-    try:
-        print "Armed: %s" % vehicle.armed
-    except:
-        print "Armed: N/A"
-
-    try:
-        print "Battery: %s" % vehicle.battery
-    except:
-        print "Battery: N/A"
-
-    try:
-        print "GPS: %s" % vehicle.gps_0
-    except:
-        print "GPS: N/A"
-
-    try:
-        print "Location: %s" %vehicle.location.global_relative_frame
-    except:
-        print "Location: N/A"
-
-    try:
-        print "Attitude: %s" % vehicle.attitude
-    except:
-        print "Attitude: N/A"
-
-    try:
-        print "Groundspeed: %s m/s" % vehicle.groundspeed
-    except:
-        print "Groundspeed: N/A"
+    print "Mode: %s" % vehicle.mode.name
+    print "Armed: %s" % vehicle.armed
+    print "Battery: %s" % vehicle.battery
+    print "GPS: %s" % vehicle.gps_0
+    print "Location: %s" %vehicle.location.global_relative_frame
+    print "Attitude: %s" % vehicle.attitude
+    print "Groundspeed: %s m/s" % vehicle.groundspeed
 
     vehicle.close()
     print "\nDone!"
